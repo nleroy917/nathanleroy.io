db7bbb8254607ea6dfb379f0fc153e462337a948
Nathan LeRoy <NLeRoy917@gmail.com>
2021-03-28 21:08:09 -0400

    more preview issues

--- src/pages/preview.js
+++ src/pages/preview.js
@@ -1,28 +1,17 @@
 import * as React from 'react'
 import { withPreviewResolver } from 'gatsby-source-prismic'
-import { graphql, useStaticQuery } from 'gatsby'
+import { linkResolver } from '../utils/linkResolver'
 
-import linkResolver from '../utils/linkResolver'
-
-const PreviewPage = ({ isPreview }) => {
-  if (isPreview === false) return 'Not a preview!'
-
-  return <p>Loading</p>
+const PreviewPage = ({ isPreview, isLoading }) => {
+  const previewText = isPreview ? 'Loading' : 'Not a preview!'
+  return (
+    <div>
+      <p>{previewText}</p>
+    </div>
+  )
 }
 
-export default (props) => {
-  const data = useStaticQuery(graphql`
-    query {
-      sitePlugin(name: { eq: "gatsby-source-prismic" }) {
-        pluginOptions {
-          repositoryName
-        }
-      }
-    }
-  `)
-  const { repositoryName } = data.sitePlugin.pluginOptions
-  return withPreviewResolver(PreviewPage, {
-    repositoryName,
-    linkResolver: () => (doc) => linkResolver(doc),
-  })(props)
-}
+export default withPreviewResolver(PreviewPage, {
+  repositoryName: 'nathanleroy-io',
+  linkResolver: () => linkResolver,
+})
