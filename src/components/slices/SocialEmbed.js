48112ef210d7b3f4578cf62cbb80d5daa4436fd0
Nathan LeRoy <NLeRoy917@gmail.com>
2021-05-08 16:03:37 -0400

    fix indent

--- src/components/slices/SocialEmbed.js
+++ src/components/slices/SocialEmbed.js
@@ -4,7 +4,7 @@ export default ({ slice }) => {
     return (
       <div >
         <div dangerouslySetInnerHTML={{ __html: slice.primary.embed_code.text }} 
-             className="w-full justify-center flex-row flex" ></div>
-      </div>
+            className="w-full justify-center flex-row flex" ></div>
+        </div>
   )
 }
