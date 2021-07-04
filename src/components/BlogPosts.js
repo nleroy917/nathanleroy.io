46ebd2b656cbc1c1d2f036b56c1e43163edf0679
Nathan LeRoy <NLeRoy917@gmail.com>
2021-07-04 19:00:42 -0400

    postcard styling

--- src/components/BlogPosts.js
+++ src/components/BlogPosts.js
@@ -21,8 +21,8 @@ const PostCard = ({ post, id }) => {
   // // Default title when post has no title set
   const defaultTitle = 'Untitled'
   return (
-    <div key={id} className="w-full">
-      <div className="bg-white shadow-lg p-5 hover:shadow-sm transition-all border-2 border-purple-600 border-opacity-0 hover:border-opacity-100">
+    <div key={id} className="w-full rounded-lg">
+      <div className="rounded-lg bg-white shadow-md p-5 hover:shadow-sm transition-all border-2 border-purple-600 border-opacity-0 hover:border-opacity-100">
        <div className="flex flex-row items-start justify-between">
         <h2 className="text-2xl font-bold">
             {RichText.asText(post.node.data.title.raw).length !== 0
@@ -30,7 +30,7 @@ const PostCard = ({ post, id }) => {
               : defaultTitle}
         </h2>
         <div>
-        <Link to={post.node.url}><button className="ml-1 lg:ml-2 xl:ml-2 bg-transparent hover:bg-black text-black hover:text-white py-1 px-4 border border-black transition-all">Read</button></Link>
+          <Link to={post.node.url}><button className="rounded-lg text-lg font-bold ml-1 lg:ml-2 xl:ml-2 bg-transparent hover:bg-black text-black hover:text-white py-1 px-4 border-2 border-black transition-all">Read</button></Link>
         </div>
        </div>
         <p className="font-light">
